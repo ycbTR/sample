120b363f306d236fdc98d7c88a7d7a95037d57c9
yigitcan bacakoglu <yigitcan_bacakoglu@hotmail.com>
2014-09-04 22:45:00 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seedbank::Application.config.secret_token = 'c2dc781769960ee3be1f93e40e5037fd041c7aca88387c2d7204f478773c50dd7d5d0c4eb6862a68e314bb962e624bdb443289d44aada3e977d5b497bca204f5'
