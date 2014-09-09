5c5705fd83a8db88978ad879649a122e7a5207ed
yigitcan bacakoglu <yigitcan_bacakoglu@hotmail.com>
2014-09-09 23:09:48 +0300

    changes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
+    
     @orders = Order.all
     respond_to do |format|
       format.html # index.html.erb
