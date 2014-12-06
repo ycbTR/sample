08e531fd496f5f4d68c8e3267f32d948e8e1dda6
yigitcan bacakoglu <yigitcan_bacakoglu@hotmail.com>
2014-12-06 22:35:21 +0200

    Adds mail settings

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,4 +1,5 @@
 class OrderMailer < ActionMailer::Base
+  default from: "SeedBank <projects@yigitbacakoglu.com>"
 
   def notify_processed(order)
     @order = order
@@ -9,14 +10,14 @@ class OrderMailer < ActionMailer::Base
 
   def new_order_to_customer(order_form)
     @order_form, @order = order_form, order_form.order
-    subject = "Received Your Order ##{@order.number}"
+    subject = "Received your order ##{@order.number}"
     mail(:to => @customer.email, :subject => subject)
   end
 
   def new_order_to_admin(order_form)
     @order_form, @order = order_form, order_form.order
     subject = "New Order ##{@order.number}"
-    mail(:to => "order@seedbank.com", :subject => subject)
+    mail(:to => Rails.env.development? ? "ycbacakoglu@gmail.com" : "w.paton@gmail.com", :subject => subject)
   end
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,20 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Seedbank::Application.initialize!
+
+
+ActionMailer::Base.smtp_settings = {
+    :address => "smtp.yandex.ru",
+    :port => 587,
+    :user_name => "projects@yigitbacakoglu.com",
+    :password => "projects123",
+    :authentication => :plain,
+    :enable_starttls_auto => true,
+    :host => "yigitbacakoglu.com"
+
+}
+
+Seedbank::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+end
