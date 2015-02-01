c4531452e22304198fa19e3a3f98991d688f8ef7
yigitcan bacakoglu <yigitcan_bacakoglu@hotmail.com>
2015-02-01 12:35:34 +0200

    Fixes timezone issue

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Seedbank
     # config.time_zone = 'Central Time (US & Canada)'
 
     config.time_zone = 'Australia/Sydney'
-    config.active_record.default_timezone = 'Australia/Sydney'
+    config.active_record.default_timezone = :utc
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
