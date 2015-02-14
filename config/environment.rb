e3574ce622d2aafb21a3e9c2a1214821e695ded8
yigitcan bacakoglu <yigitcan_bacakoglu@hotmail.com>
2015-02-14 18:48:46 +0200

    fix

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Seedbank::Application.initialize!
 
+Setting.set_email_settings
+
 Seedbank::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
