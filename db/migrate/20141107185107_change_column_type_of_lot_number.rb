7499f4c5a0654760b470d6dbc7ae59285782b24f
Divyansh <divyansh.kumar@cryptextechnologies.com>
2015-03-23 12:22:54 +0530

    ChangeColumnTypeOfLotNumber correction (type cast error in pg)

--- db/migrate/20141107185107_change_column_type_of_lot_number.rb
+++ db/migrate/20141107185107_change_column_type_of_lot_number.rb
@@ -1,5 +1,5 @@
 class ChangeColumnTypeOfLotNumber < ActiveRecord::Migration
   def change
-    change_column :lot_numbers, :number, :integer
+    change_column :lot_numbers, :number, 'integer USING CAST(number AS integer)'
   end
 end
