55295897e4f45583872ab66f728a559ff34c227e
satyakaam <satyakaam.parikh@gmail.com>
2016-05-04 05:56:40 +0530

    issue with deposit adjustment

--- app/controllers/admin/resource_controller.rb
+++ app/controllers/admin/resource_controller.rb
@@ -56,11 +56,13 @@ class Admin::ResourceController < Admin::BaseController
         format.html { redirect_to location_after_save }
         format.js { js_response }
       end
-      if (@object.type == "Person::Collector" || @object.type == "Person::Customer") && @object.email != ""
-        u = User.create(email: @object.email, password: "12345678", password_confirmation: "12345678", role: @object.type.split("::").last)
-        u.send_reset_password_instructions
-        @object.user_id = u.id
-        @object.save
+      if @object.respond_to? :type
+        if (@object.type == "Person::Collector" || @object.type == "Person::Customer") && @object.email != ""
+          u = User.create(email: @object.email, password: "12345678", password_confirmation: "12345678", role: @object.type.split("::").last)
+          u.send_reset_password_instructions
+          @object.user_id = u.id
+          @object.save
+        end
       end
     else
       @error_presence_for_response = true
