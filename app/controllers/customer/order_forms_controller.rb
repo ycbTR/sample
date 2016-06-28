b9c5f9e0a32deeed381c70588a625934c1d54eef
satyakaam <satyakaam.parikh@gmail.com>
2016-06-28 09:51:46 +0530

    minor

--- app/controllers/customer/order_forms_controller.rb
+++ app/controllers/customer/order_forms_controller.rb
@@ -48,15 +48,17 @@ class Customer::OrderFormsController < Customer::BaseController
       # if Rails.env.production?
         # plant_ids = Deposit.seeding.active.pluck(:plant_id).uniq
         # @plants = Plant.where(id: plant_ids).order("plants.species asc")
-        ids = Deposit.active.with_eager_load.available.where("#{Plant.table_name}.direct_seedable = ?",true).select("DISTINCT ON(deposits.plant_id) *").pluck(:id)
-        @deposits = Deposit.where(id: ids).joins(:lot_number,:plant).where(lot_numbers: {spa_specific: false}).order('plants.species asc')
+        # ids = Deposit.active.with_eager_load.available.where("#{Plant.table_name}.direct_seedable = ?",true).select("DISTINCT ON(deposits.plant_id) *").pluck(:id)
+        # @deposits = Deposit.where(id: ids).joins(:lot_number,:plant).where(lot_numbers: {spa_specific: false}).order('plants.species asc')
+        @deposits = Deposit.active.seeding.where(lot_numbers: {spa_specific: false}).order('plants.species asc')
         
       # else
       #   @deposits = Deposit.seeding.active.group(:plant_id).uniq
       # end
     elsif @current_order.type == "OrderForm::Spa"
-      ids = Deposit.active.with_eager_load.available.where("#{Plant.table_name}.direct_seedable = ?",true).select("DISTINCT ON(deposits.plant_id) *").pluck(:id)
-      @deposits = Deposit.where(id: ids).joins(:lot_number,:plant).where(lot_numbers: {spa_specific: true}).order('plants.species asc')
+      @deposits = Deposit.active.seeding.where(lot_numbers: {spa_specific: true}).order('plants.species asc')
+      # ids = Deposit.active.with_eager_load.available.where("#{Plant.table_name}.direct_seedable = ?",true).select("DISTINCT ON(deposits.plant_id) *").pluck(:id)
+      # @deposits = Deposit.where(id: ids).joins(:lot_number,:plant).where(lot_numbers: {spa_specific: true}).order('plants.species asc')
     else
       @deposits = Deposit.nursery.active
       @deposits = @deposits.order("plants.species asc")
